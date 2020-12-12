1f0f0acfd6bcf50db15aba93b8f2d63558461dc4
Joe Meyer <j23meyerj@gmail.com>
2020-12-12 17:04:49 -0600

    Saving changes for on click, monthly total amount and color, and delete functionality.

--- client.js
+++ client.js
@@ -8,29 +8,47 @@ function readyNow() {
 
     // click listeners
     $('#addEmployee').on('click', employeeClick);
+    $('#employeeData').on('click', '#deleteEmployee', employeeDataDelete);
+
 }
 
 
 let employees = [];
 
+//let totalMonthly = sumMonthly(employees);
+// console.log('total monthly sum', totalMonthly);
 
-function employeeClick(){
+
+
+function employeeClick() {
     console.log('in employeeClick');
     let newEmployee = {
         firstName: $('#firstNameIn').val(),
         lastName: $('#lastNameIn').val(),
         idNumber: $('#idNumberIn').val(),
         jobTitle: $('#jobTitleIn').val(),
-        annualSalary: $('#annualSalaryIn').val()     
+        annualSalary: $('#annualSalaryIn').val()
     };
-    console.log(newEmployee);
+    console.log('new employee info', newEmployee);
+
     employees.push(newEmployee);
-    console.log(employees);
+    console.log('array', employees);
+
+
 
     renderToDom();
 
+
+
     clearInput();
 
+    sumMonthly(employees);
+
+}
+
+function employeeDataDelete() {
+    console.log('in data delete');
+    $(this).parent().parent().remove()
 }
 
 
@@ -45,16 +63,49 @@ function renderToDom() {
            <td>${employee.idNumber}</td>
            <td>${employee.jobTitle}</td>
            <td>${employee.annualSalary}</td>
-           <button>Delete</button>
+           <td><button class="delete employee"  id="deleteEmployee">Delete</button></td>
         </tr>`)
-        //$('#employeeData').priceFormat(`
-        //<tr class="employee">
-           //<td>${employee.annualSalary}</td>
-        //</tr>`)
+
     }
+
+
 }
 
-function clearInput(){
+
+function sumMonthly(array) {
+    console.log('in sumMonthly');
+    let sum = 0;
+    for (let employee of array) {
+        console.log('in for loop');
+        sum += parseFloat(employee.annualSalary);
+        console.log('employee.annualSalary', employee.annualSalary);
+        console.log('sum', sum);
+    }
+    let total = sum / 12
+    let n = total.toFixed(2);
+    total = n
+    console.log('total', total);
+    let totalSalary = $('#annualSalaryAdder')
+    totalSalary.empty();
+    
+    totalSalary.append(`<div id=annualSalaryAdder>Total Monthly: ${total}</div>`);
+    console.log('totalSalary', totalSalary);
+
+    
+
+    if (total > 20000.00) {
+        console.log('in if', total);
+        $('#annualSalaryAdder').css("background-color", "red");
+    }
+    return total;
+}
+
+
+
+
+
+
+function clearInput() {
     console.log('in clearInput');
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
@@ -65,4 +116,3 @@ function clearInput(){
 
 
 
-//$('#price').priceFormat();
